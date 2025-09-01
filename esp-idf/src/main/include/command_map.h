1fb3ef227a2ed50f73386733143ae6d051bc7515
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2025-09-01 19:14:26 +0800

    update custom map

--- esp-idf/src/main/include/command_map.h
+++ esp-idf/src/main/include/command_map.h
@@ -43,11 +43,150 @@ spi_oled_animation_t anim_go_straight = {
     .task_handle = NULL
 };
 
+spi_oled_animation_t anim_parking = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 30,
+    .animation_data = (const uint8_t *)parking,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_wait = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 10,
+    .animation_data = (const uint8_t *)wait,
+    .frame_delay_ms = 1000 / 15,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_hi = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 30,
+    .animation_data = (const uint8_t *)hi,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_check_mark = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 32,
+    .animation_data = (const uint8_t *)check_mark,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_help_sos = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 5,
+    .animation_data = (const uint8_t *)help_sos,
+    .frame_delay_ms = 1000 / 15,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_up_hill = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 30,
+    .animation_data = (const uint8_t *)up_hill,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_down_hill = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 30,
+    .animation_data = (const uint8_t *)down_hill,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_slow = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 30,
+    .animation_data = (const uint8_t *)slow,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_attention = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 30,
+    .animation_data = (const uint8_t *)attention,
+    .frame_delay_ms = 1000 / 30,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
+
+spi_oled_animation_t anim_walk = {
+    .x = 14,
+    .y = 14,
+    .width = 100,
+    .height = 100,
+    .frame_count = 8,
+    .animation_data = (const uint8_t *)walk,
+    .frame_delay_ms = 1000 / 15,
+    .stop_frame = -1,
+    .reverse = false,
+    .task_handle = NULL
+};
 
 static animation_map_entry_t animation_map[] = {
     {1, &anim_turn_left},
     {2, &anim_turn_right},
     {3, &anim_go_straight},
+    {4, &anim_parking},
+    {5, &anim_wait},
+    {6, &anim_hi},
+    {7, &anim_check_mark},
+    {8, &anim_help_sos},
+    {9, &anim_up_hill},
+    {10, &anim_down_hill},
+    {11, &anim_slow},
+    {12, &anim_attention},
+    {13, &anim_walk},
     {-1, NULL} // Sentinel to mark end of array
 };
 
